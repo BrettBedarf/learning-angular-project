46772c66746d33d1f1e094d2d6a24a12d21b16ae
Brett Bedarf <brettbedarf.dev@gmail.com>
2021-03-18 21:44:44 -0600

    Display all characters as items in list

--- src/app/tabs/tabs.component.ts
+++ src/app/tabs/tabs.component.ts
@@ -3,13 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.component.html',
-  styleUrls: ['./tabs.component.css']
+  styleUrls: ['./tabs.component.css'],
 })
 export class TabsComponent implements OnInit {
+  characters = [
+    { name: 'Luke Skywalker', side: '' },
+    { name: 'Darth Vader', side: '' },
+  ];
+  activeList = 'all';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getCharacters() {
+    return this.characters.filter(
+      (c) => c.side === this.activeList || this.activeList === 'all'
+    );
   }
-
+  onChoose(tabName: string) {
+    this.activeList = tabName;
+  }
 }
+
+// let tab = new TabsComponent();
