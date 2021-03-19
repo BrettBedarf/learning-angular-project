cd2af1d6c4cd60ec2b595d920d1021182c7d671e
Brett Bedarf <brettbedarf.dev@gmail.com>
2021-03-19 11:45:59 -0600

    Inject LogService into StarWarsService

--- src/app/star-wars.service.ts
+++ src/app/star-wars.service.ts
@@ -1,16 +1,28 @@
+import { LogService } from './log.service';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
 export class StarWarsService {
   private characters = [
     { name: 'Luke Skywalker', side: '' },
     { name: 'Darth Vader', side: '' },
   ];
+  private logService: LogService;
+
+  constructor(logService: LogService) {
+    this.logService = logService;
+  }
 
   getCharacters(activeList: string) {
     return this.characters.filter(
       (c) => c.side === activeList || activeList === 'all'
     );
   }
-  onSideChanged(charInfo) {
+  onSideChanged(charInfo: { name: string; side: string }) {
     const charToChange = this.characters.find((c) => c.name === charInfo.name);
     charToChange.side = charInfo.side;
+    this.logService.writeLog(
+      `Changed side of${charInfo.name} to ${charInfo.side}`
+    );
   }
 }
